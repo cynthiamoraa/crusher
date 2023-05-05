c4d7d223ce3259f81d0d79acb83aa3b4a9418f92
Kimutai01 <cmoraa733@gmail.com>
2023-05-05 10:55:58 +0300

    new changes

--- src/Nah.cjs
+++
@@ -1,7 +0,0 @@
-const nah = document.getElementById('nah');
-const yah = document.getElementById("yah");
-
-nah.addEventListener('click', () => {
-    console.log('nah');
-});
-module.exports = nah;
